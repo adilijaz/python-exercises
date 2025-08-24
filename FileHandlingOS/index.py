92299b0394b66cc6cdafa670cf9e4265355de06f
adilijaz <adilijaz.02@gmail.com>
2025-08-24 16:34:32 +0500

    File Handling

--- FileHandlingOS/index.py
+++ FileHandlingOS/index.py
@@ -2,5 +2,20 @@
 file_path = 'doc.txt'
 with open(file_path, 'r') as file:
     contents = file.read()
-    print(contents) 
+    # print(contents) 
+
+
+# Reads a .txt file and prints the top 5 most common words.
+from collections import Counter
+import re   
+file_path = 'doc.txt'
+with open(file_path, 'r') as file:
+    contents = file.read().lower()
+    words = re.findall(r'\b\w+\b', contents)
+    word_counts = Counter(words)
+    most_common_words = word_counts.most_common(5)
+    print(most_common_words)
+
+
+
 
